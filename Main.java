299378e7fbdb69e88b82d62bc2954c156430b58a
jimatlang <jim.stewart@thelangschool.org>
2024-03-08 10:16:57 -0500

    added test cases from main branch

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ public class Main {
         new Main();
     }
     public Main(){
-        Digits d = new Digits(1234567899);
-        System.out.println(d.isStrictlyIncreasing());
+        Digits d1 = new Digits(123456789);
+        System.out.println(d1.isStrictlyIncreasing());
+        Digits d2 = new Digits(1234567789);
+        System.out.println(d2.isStrictlyIncreasing());
+        Digits d3 = new Digits(123456543);
+        System.out.println(d3.isStrictlyIncreasing());
     }
 }
