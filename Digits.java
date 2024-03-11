f3cd196c7b89277a4452a4b452a4a04fd90fdfed
jimatlang <jim.stewart@thelangschool.org>
2024-03-11 13:55:10 -0400

    digits constructor and isStrictlyIncreasing

--- Digits.java
+++ Digits.java
@@ -2,8 +2,18 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Digits {
-    private ArrayList<Integer> digits;
+    private ArrayList<Integer> digits; //an empty space where the arraylist will go
     public Digits(int number){
+        digits = new ArrayList<>(); //puts an empty arraylist in the empty space
+        // this will put the digits backward!
+        while(number > 0){
+            digits.add(number%10);
+            number = number/10;
+        }
+        // debug lines:
+        // for(int digit:digits){
+        //     System.out.println(digit);
+        // }
     }
     public ArrayList<Integer> getDigits() {
         return digits;
@@ -15,6 +25,16 @@ public class Digits {
         return digits.get(n);
     }
     public boolean isStrictlyIncreasing(){
-        return false;
+        int n = digits.size()-1;//position of the last item in list
+        int biggest = 0;
+        while(n>=0){
+            if(digits.get(n)>biggest){
+                biggest = digits.get(n);
+                n-=1;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
